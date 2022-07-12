ac7f2abc8a252a67fc5cb781ded101dc911819e7
Jinquan Wang <w1637894214@163.com>
2022-07-12 20:55:22 +0800

    feat: add lualine

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -3,3 +3,9 @@ local conf = require('modules.ui.configs')
 
 register({ 'glepnir/zephyr-nvim', config = conf.zephyr })
 
+register({
+  'nvim-lualine/lualine.nvim',
+  config = conf.lualine,
+  requires = { 'kyazdani42/nvim-web-devicons' }
+})
+
