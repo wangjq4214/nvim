c5e6ccf37c19a92956cf8dda63a5e2fce02b7abf
Jinquan Wang <w1637894214@163.com>
2022-10-02 19:55:45 +0800

    ✨ feat: a new keymap method

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,5 @@
 require('core.options')
+require('core.keymap')
 
 local packer = require('core.packer')
 
--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,8 +1,22 @@
+local uv, fn = vim.loop, vim.fn
+local util = require('util')
+
+local function read_file()
+  local path = util:join_paths(fn.stdpath('config'), "keymap.json")
+  local fd = assert(uv.fs_open(path, "r", tonumber("0444", 8)))
+
+  local stat = assert(uv.fs_fstat(fd))
+  local buf = assert(uv.fs_read(fd, stat.size, 0))
+  uv.fs_close(fd)
+
+  return buf
+end
+
 local opts = {}
 
 -- init a option instance
-function opts:new(instance)
-  instance = instance or {
+function opts:builder()
+  local instance = {
     options = {
       silent = false,
       nowait = false,
@@ -17,106 +31,85 @@ function opts:new(instance)
   return instance
 end
 
-local M = {}
-
 -- a part of build pattern to set attr to opt
-function M.silent(opt)
-  return function()
-    opt.silent = true
-  end
+function opts:silent()
+  self.options.silent = true
 end
 
 -- a part of build pattern to set attr to opt
-function M.noremap(opt)
-  return function()
-    opt.noremap = true
-  end
+function opts:noremap()
+  self.options.noremap = true
 end
 
 -- a part of build pattern to set attr to opt
-function M.expr(opt)
-  return function()
-    opt.expr = true
-  end
+function opts:expr()
+  self.options.expr = true
 end
 
 -- a part of build pattern to set attr to opt
-function M.remap(opt)
-  return function()
-    opt.remap = true
-  end
+function opts:nowait()
+  self.options.nowait = true
 end
 
--- a part of build pattern to set attr to opt
-function M.nowait(opt)
-  return function()
-    opt.nowait = true
-  end
-end
-
--- exec build function to build a map option
-function M.new_opts(...)
-  local args = { ... }
-  local o = opts:new()
-
-  if #args == 0 then
-    return o.options
-  end
-
-  for _, arg in pairs(args) do
-    arg(o.options)()
-  end
-  return o.options
+function opts:build()
+  return self.options
 end
 
 -- a wrap to exec cmd in command mode
-function M.cmd(str)
+local function cmd(str)
   return '<cmd>' .. str .. '<CR>'
 end
 
 -- a wrap to exec operate visual mode
-function M.cu(str)
+local function cu(str)
   return ':<C-u>' .. str .. '<CR>'
 end
 
--- @private
-local keymap_set = function(mode, tbl)
-  vim.validate({
-    tbl = { tbl, 'table' }
-  })
-
-  local len = #tbl
-  if len < 2 then
-    vim.notify('keymap must has rhs')
+local function load_keymap_config()
+  local data = read_file()
+  local keymap_data = fn.json_decode(data)
+  if keymap_data == nil then
     return
   end
 
-  local options = len == 3 and tbl[3] or M.new_opts()
-  vim.keymap.set(mode, tbl[1], tbl[2], options)
-end
-
--- @private
-local function map(mod)
-  return function(tbl)
-    vim.validate({
-      tbl = { tbl, 'table' }
-    })
-
-    if type(tbl[1]) == 'table' and type(tbl[2]) == 'table' then
-      for _, v in pairs(tbl) do
-        keymap_set(mod, v)
-      end
-    else
-      keymap_set(mod, tbl)
+  for k, v in pairs(keymap_data) do
+    local mode = v.mode
+    if mode == nil then
+      mode = 'n'
     end
+
+    repeat
+      local rhs
+      if v.cmd ~= nil then
+        rhs = cmd(v.cmd)
+      end
+
+      if rhs == nil then
+        break
+      end
+
+      local builer = opts:builder()
+      if v.option ~= nil then
+        if v.option.silent then
+          builer:silent()
+        end
+
+        if v.option.nowait then
+          builer:nowait()
+        end
+
+        if v.option.expr then
+          builer:expr()
+        end
+
+        if v.option.noremap then
+          builer:noremap()
+        end
+      end
+
+      vim.keymap.set(mode, k, rhs, builer:build())
+    until true
   end
 end
 
-M.nmap = map('n')
-M.imap = map('i')
-M.cmap = map('c')
-M.vmap = map('v')
-M.xmap = map('x')
-M.tmap = map('t')
-
-return M
+load_keymap_config()
