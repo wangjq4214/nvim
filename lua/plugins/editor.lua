cef22e8214eb1ad0dd77a654800f0454aa91bf7e
wangjq4214 <w1637894214@163.com>
2023-03-01 16:31:17 +0800

    feat: add telescope, treesitter and leap

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,24 @@
+function telescope_util(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+
+  return function()
+    builtin = params.builtin
+    opts = params.opts
+    opts = vim.tbl_deep_extend('force', { cwd = require('utils').get_root() }, opts or {})
+
+    if builtin == 'files' then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. '/.git') then
+        opts.show_untracked = true
+        builtin = 'git_files'
+      else
+        builtin = 'find_files'
+      end
+    end
+
+    return require('telescope.builtin')[builtin](opts)
+  end
+end
+
 return {
   -- file explorer
   {
@@ -63,6 +84,116 @@ return {
         desc = 'Replace in files (Spectre)'
       }
     },
+  },
+  -- fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    cmd = 'Telescope',
+    version = false,
+    keys = {
+      { '<leader>,', '<cmd>Telescope buffers show_all_buffers=true<cr>', desc = 'Switch Buffer' },
+      { '<leader>/', telescope_util('live_grep'), desc = 'Find in Files (Grep)' },
+      { '<leader>:', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
+      { '<leader><space>', telescope_util('files'), desc = 'Find Files (root dir)' },
+      -- find
+      { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
+      { '<leader>ff', telescope_util('files'), desc = 'Find Files (root dir)' },
+      { '<leader>fF', telescope_util('files', { cwd = false }), desc = 'Find files (cwd)' },
+      { '<leader>fr', '<cmd>Telescope old files<cr>', desc = 'Recent' },
+      -- git
+      { '<leader>gc', '<cmd>Telescope git_commits<cr>', desc = 'Commits' },
+      { '<leader>gs', '<cmd>Telescope git_status<cr>', desc = 'Status' },
+      -- search
+      { '<leader>sa', '<cmd>Telescope autocommands<cr>', desc = 'Auto command' },
+      { '<leader>sb', '<cmd>Telescope current_buffer_fuzzy_find<cr>', desc = 'Buffer' },
+      { '<leader>sc', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
+      { '<leader>sC', '<cmd>Telescope commands<cr>', desc = 'Commands' },
+      { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = 'Diagnostics' },
+      { '<leader>sg', telescope_util('live_grep'), desc = 'Grep (root dir)' },
+      { '<leader>sG', telescope_util('live_grep', { cwd = false }), desc = 'Grep (cwd)' },
+      { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = 'Help Pages' },
+      { '<leader>sH', '<cmd>Telescope highlights<cr>', desc = 'Search Highlight Groups' },
+      { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = 'Key Maps' },
+      { '<leader>sM', '<cmd>Telescope man_pages<cr>', desc = 'Man Pages' },
+      { '<leader>sm', '<cmd>Telescope marks<cr>', desc = 'Jump to Mark' },
+      { '<leader>so', '<cmd>Telescope vim_options<cr>', desc = 'Options' },
+      { '<leader>sw', telescope_util('grep_string'), desc = 'Word (root dir)' },
+      { '<leader>sW', telescope_util('grep_string', { cwd = false }), desc = 'Word (cwd)' },
+      { '<leader>uC', telescope_util('colorscheme', { enable_preview = true }), desc = 'Colorscheme with preview' },
+      {
+        '<leader>ss',
+        telescope_util('lsp_document_symbols', {
+          symbols = {
+            'Class',
+            'Function',
+            'Method',
+            'Constructor',
+            'Interface',
+            'Module',
+            'Struct',
+            'Trait',
+            'Field',
+            'Property',
+          },
+        }),
+        desc = 'Goto Symbol',
+      },
+    },
+    opts = {
+      defaults = {
+        prompt_prefix = ' ',
+        selection_caret = ' ',
+        mappings = {
+          i = {
+            ['<c-t>'] = function(...)
+              return require('trouble.providers.telescope').open_with_trouble(...)
+            end,
+            ['<a-i>'] = function()
+              telescope_util('find_files', { no_ignore = true })()
+            end,
+            ['<a-h>'] = function()
+              telescope_util('find_files', { hidden = true })()
+            end,
+            ['<C-Down>'] = function(...)
+              return require('telescope.actions').cycle_history_next(...)
+            end,
+            ['<C-Up>'] = function(...)
+              return require('telescope.actions').cycle_history_prev(...)
+            end,
+            ['<C-f>'] = function(...)
+              return require('telescope.actions').preview_scrolling_down(...)
+            end,
+            ['<C-b>'] = function(...)
+              return require('telescope.actions').preview_scrolling_up(...)
+            end,
+          },
+          n = {
+            ['q'] = function(...)
+              return require('telescope.actions').close(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+  -- easily jump to any location and enhanced f/t motions for Lead
+  {
+    'ggandor/leap.nvim',
+    event = 'VeryLazy',
+    dependencies = { { 'ggandor/flit.nvim', opts = { labeled_modes = 'nv' } } },
+    config = function(_, opts)
+      local leap = require('leap')
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
+    end,
+  },
+  -- git signs
+  {
+    'lewis6991/gitsigns.nvim',
   },
 }
 
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    version = false,
+    build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufNewFile' },
+    keys = {
+      { '<c-space>', desc = 'Increment selection' },
+      { '<bs>', desc = 'Schrink selection', mode = 'x' },
+    },
+    ---@type TSConfig
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      ensure_installed = {
+        'bash',
+        'help',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'regex',
+        'tsx',
+        'typescript',
+        'vim',
+        'yaml',
+        'c',
+        'cmake',
+        'cpp',
+        'css',
+        'diff',
+        'fish',
+        'go',
+        'gosum',
+        'gomod',
+        'gowork',
+        'graphql',
+        'http',
+        'java',
+        'jsdoc',
+        'jsonc',
+        'meson',
+        'ninja',
+        'rust',
+        'scss',
+        'svelte',
+        'toml',
+        'vue',
+        'dockerfile',
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = '<nop>',
+          node_decremental = '<bs>',
+        },
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end
+  },
+}
+
