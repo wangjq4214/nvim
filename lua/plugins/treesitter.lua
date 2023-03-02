756566739220824b8278b901a1da42b81d7e32d2
wangjq4214 <w1637894214@163.com>
2023-03-02 21:47:03 +0800

    feat: add textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,29 @@ return {
     version = false,
     build = ':TSUpdate',
     event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        init = function()
+          local plugin = require('lazy.core.config').spec.plugins['nvim-treesitter']
+          local opts = require('lazy.core.plugin').values(plugin, 'opts', false)
+          local enabled = false
+
+          if opts.textobjects then
+            for _, mod in ipairs({ 'move', 'select', 'swap', 'lsp_interop' }) do
+              if opts.textobjects[mod] and opts.textobjects[mod].enable then
+                enable = true
+                break
+              end
+            end
+          end
+
+          if not enable then
+            require('lazy.core.loader').disable_rtp_plugin('nvim-tree-sitter-textobjects')
+          end
+        end,
+      },
+    },
     keys = {
       { '<c-space>', desc = 'Increment selection' },
       { '<bs>', desc = 'Schrink selection', mode = 'x' },
