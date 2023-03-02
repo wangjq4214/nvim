05af54a9bd117e16e2e5ae59610c57525eb44fcb
wangjq4214 <w1637894214@163.com>
2023-03-02 22:15:31 +0800

    feat: add a dashboard

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -148,6 +148,9 @@ return {
             ['<c-t>'] = function(...)
               return require('trouble.providers.telescope').open_with_trouble(...)
             end,
+            ['<a-t>'] = function(...)
+              return require('trouble.providers.telescope').open_selected_with_trouble(...)
+            end,
             ['<a-i>'] = function()
               telescope_util('find_files', { no_ignore = true })()
             end,
@@ -180,7 +183,24 @@ return {
   {
     'ggandor/leap.nvim',
     event = 'VeryLazy',
-    dependencies = { { 'ggandor/flit.nvim', opts = { labeled_modes = 'nv' } } },
+    dependencies = {
+      {
+        'ggandor/flit.nvim',
+        keys = function()
+          local ret = {}
+          for _, key in ipairs({ 'f', 'F', 't', 'T' }) do
+            ret[#ret + 1] = { key, mode = { 'n', 'x', 'o' }, desc = key }
+          end
+          return ret
+        end,
+        opts = { labeled_modes = 'nx' }
+      }
+    },
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap forward to' },
+      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap backward to' },
+      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from windows' },
+    },
     config = function(_, opts)
       local leap = require('leap')
       for k, v in pairs(opts) do
@@ -231,19 +251,30 @@ return {
     'RRethy/vim-illuminate',
     event = { 'BufReadPost', 'BufNewFile' },
     opts = { delay = 200 },
-    config = function()
+    config = function(_, opts)
       require('illuminate').configure(opts)
+
+      local function map(key, dir, buffer)
+        vim.keymap.set('n', key, function()
+          require('illuminate')['goto_' .. dir .. '_reference'](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. ' Reference', buffer = buffer })
+      end
+
+      map(']]', 'next')
+      map('[[', 'prev')
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
       vim.api.nvim_create_autocmd('FileType', {
         callback = function()
           local buffer = vim.api.nvim_get_current_buf()
-          pcall(vim.keymap.del, 'n', ']]', { buffer = buffer })
-          pcall(vim.keymap.del, 'n', '[[', { buffer = buffer })
+          map(']]', 'next', buffer)
+          map('[[', 'prev', buffer)
         end,
       })
     end,
     keys = {
-      { ']]', function() require('illuminate').goto_next_reference(false) end, desc = 'Next Reference' },
-      { '[[', function() require('illuminate').goto_prev_reference(false) end, desc = 'Prev Reference' },
+      { ']]', desc = 'Next Reference' },
+      { '[[', desc = 'Prev Reference' },
     },
   },
   -- buffer remove
@@ -264,6 +295,28 @@ return {
       { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Worksapce Diagnostics (Trouble)' },
       { '<leader>xL', '<cmd>TroubleToggle loclist<cr>', desc = 'Location List (Trouble)' },
       { '<leader>xQ', '<cmd>TroubleToggle quickfix<cr>', desc = 'Quickfix List (Trouble)' },
+      {
+        '[q',
+        function()
+          if require('trouble').is_open() then
+            require('trouble').previous({ skip_groups = true, jump = true })
+          else
+            vim.cmd.cprev()
+          end
+        end,
+        desc = 'Previous Trouble/Quickfix Item',
+      },
+      {
+        ']q',
+        function()
+          if require('trouble').is_open() then
+            require('trouble').next({ skip_groups = true, jump = true })
+          else
+            vim.cmd.cnext()
+          end
+        end,
+        desc = 'Next Trouble/Quickfix Item',
+      },
     },
   },
   -- todo comments
