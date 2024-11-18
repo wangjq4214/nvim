f2122529ba832d9cdd4a598a17e88ca1d48da1f2
wangjq4214 <w1637894214@163.com>
2024-11-18 20:37:32 +0800

    feat: add support for frontend

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,8 @@ return {
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.typescript" },
   -- Motion import
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
