ad58bae3f4eb010f5aed5c9928dc9c990c19fd7f
wangjq4214 <w1637894214@163.com>
2025-05-19 15:51:52 +0800

    feat: support mini.map and mini.comment

--- lua/community.lua
+++ lua/community.lua
@@ -17,4 +17,8 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.leap-nvim" },
+  -- editor
+  { import = "astrocommunity.comment.mini-comment" },
+  -- map
+  { import = "astrocommunity.split-and-window.mini-map" },
 }
