25a8e94f466659865339e3a1566bebcc2bd6db29
wangjq4214 <w1637894214@163.com>
2025-09-04 15:40:10 +0800

    chore: one markdown render is better

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,30 +1,11 @@
 ---@type LazySpec
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  cmd = "RenderMarkdown",
-  ft = function()
-    local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
-    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-    return opts.file_types or { "markdown" }
-  end,
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        if opts.ensure_installed ~= "all" then
-          opts.ensure_installed =
-            require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "markdown", "markdown_inline" })
-        end
-      end,
+  "OXY2DEV/markview.nvim",
+  ft = { "markdown", "quarto", "rmd", "typst", "norg", "org", "vimwiki", "Avante" },
+  opts = {
+    preview = {
+      filetypes = { "markdown", "quarto", "rmd", "typst", "norg", "org", "vimwiki", "Avante" },
+      ignore_buftypes = {},
     },
-    "echasnovski/mini.icons",
   },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
-  config = function()
-    require("render-markdown").setup {
-      completions = { blink = { enabled = true } },
-    }
-  end,
 }
