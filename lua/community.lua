376b769fe04c79d2e8acd934584bc4f37833607a
wangjq4214 <w1637894214@163.com>
2025-10-09 19:44:59 +0800

    ⚡ feat: change from leap.nvim to flash.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -16,7 +16,7 @@ return {
   -- motion
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
   -- editor
   { import = "astrocommunity.comment.mini-comment" },
   -- ai
