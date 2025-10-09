440f7544ed1d905ead833ab78522770ac5ce5043
wangjq4214 <w1637894214@163.com>
2025-10-09 20:28:07 +0800

    feat: update the deps and enhance the sidekick

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -14,7 +14,10 @@ return {
     {
       "<tab>",
       function()
-        if not require("sidekick").nes_jump_or_apply() then return "<Tab>" end
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require("sidekick").nes_jump_or_apply() then
+          return "<Tab>" -- fallback to normal tab
+        end
       end,
       expr = true,
       desc = "Goto/Apply Next Edit Suggestion",
@@ -45,9 +48,87 @@ return {
     },
     {
       "<leader>ap",
-      function() require("sidekick.cli").select_prompt() end,
+      function() require("sidekick.cli").prompt() end,
       desc = "Sidekick Ask Prompt",
       mode = { "n", "v" },
+    },
+  },
+  dependencies = {
+    {
+      "saghen/blink.cmp",
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+
+        keymap = {
+          ["<Tab>"] = {
+            "snippet_forward",
+            function() -- sidekick next edit suggestion
+              return require("sidekick").nes_jump_or_apply()
+            end,
+            -- function() -- if you are using Neovim's native inline completions
+            --   return vim.lsp.inline_completion.get()
+            -- end,
+            "fallback",
+          },
+        },
+      },
+    },
+    {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require "astroui.status"
+
+        local sidekick_component = status.component.builder {
+          {
+            provider = function() return " " end,
+          },
+          condition = function()
+            local ok, sk = pcall(require, "sidekick.status")
+            return ok and sk.get() ~= nil
+          end,
+          hl = function()
+            local get_hl = require("astroui").get_hlgroup
+            local sk = require("sidekick.status").get()
+
+            local grp = "Special"
+            if sk then
+              if sk.kind == "Error" then
+                grp = "DiagnosticError"
+              elseif sk.busy then
+                grp = "DiagnosticWarn"
+              end
+            end
+            return { fg = get_hl(grp).fg }
+          end,
+          surround = { separator = "right" },
+        }
+
+        opts.statusline = {
+          hl = { fg = "fg", bg = "bg" },
+
+          status.component.mode(),
+          status.component.git_branch(),
+          status.component.file_info(),
+          status.component.git_diff(),
+          status.component.diagnostics(),
+
+          status.component.fill(),
+
+          status.component.cmd_info(),
+
+          status.component.fill(),
+
+          status.component.lsp(),
+          status.component.virtual_env(),
+          status.component.treesitter(),
+
+          sidekick_component,
+
+          status.component.nav(),
+          status.component.mode { surround = { separator = "right" } },
+        }
+      end,
     },
   },
 }
